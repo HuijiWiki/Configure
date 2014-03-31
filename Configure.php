53877e2cf61ce07c7a1efef531112b9892dd77d3
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-31 09:49:11 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: Id3217c1ac2f17204848307009a94f89178016b75

--- Configure.php
+++ Configure.php
@@ -17,7 +17,7 @@ $wgExtensionCredits['specialpage'][] = array(
 	'author' => array( 'Alexandre Emsenhuber', 'Andrew Garrett' ),
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Configure',
 	'descriptionmsg' => 'configure-desc',
-	'version' => '0.17.3',
+	'version' => '0.18.0',
 );
 
 # Configuration part
@@ -207,6 +207,7 @@ $dir = dirname( __FILE__ ) . '/';
 require_once( $dir . 'Configure.func.php' );
 
 # Adding internationalisation...
+$wgMessagesDirs['Configure'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['Configure'] = $dir . 'Configure.i18n.php';
 $wgExtensionMessagesFiles['ConfigureSettings'] = $dir . 'settings/Settings.i18n.php';
 
